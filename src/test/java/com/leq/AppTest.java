851be6761cf6cf848b000a39f1d970d46a0a36fc
runoob <test@runoob.com>
2019-03-28 23:55:40 +0800

    2019-3-29 刘恩奇 项目的创建。。。

---
+++ src/test/java/com/leq/AppTest.java
@@ -0,0 +1,20 @@
+package com.leq;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
